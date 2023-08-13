f49a5a873456e1fcc36ac9f2897929f33382dbf8
deets <deets@web.de>
2023-08-13 19:51:05 +0200

    Two SMs outputting data

--- src/main.rs
+++ src/main.rs
@@ -40,39 +40,68 @@ fn main() -> ! {
 
     // configure LED pin for Pio0.
     let _led: Pin<_, FunctionPio0> = pins.gpio17.into_mode();
+    let _led: Pin<_, FunctionPio0> = pins.gpio18.into_mode();
     // PIN id for use inside of PIO
     let led_pin_id = 17;
+    let other_pin = 18;
+
+    // // Define some simple PIO program.
+    // let program = pio_proc::pio_asm!(
+    //     "pull block",
+    //     "mov x, osr",
+    //     ".wrap_target",
+    //     "mov y, x",
+    //     "set pins, 1",
+    //     "high:",
+    //     "jmp y-- high",
+    //     "mov y, x",
+    //     "set pins, 0",
+    //     "low:",
+    //     "jmp y-- low",
+    //     ".wrap"
+    // );
 
     // Define some simple PIO program.
-    let program = pio_proc::pio_asm!(
-        "pull block",
-        "mov x, osr",
+    let short_high = pio_proc::pio_asm!(
         ".wrap_target",
-        "mov y, x",
-        "set pins, 1",
-        "high:",
-        "jmp y-- high",
-        "mov y, x",
-        "set pins, 0",
-        "low:",
-        "jmp y-- low",
+        "set pins, 1 [3]",
+        "set pins, 0 [31]",
         ".wrap"
     );
 
+
+    let long_high = pio_proc::pio_asm!(
+        ".wrap_target",
+        "set pins, 1 [31]",
+        "set pins, 0 [3]",
+        ".wrap"
+    );
+
+
     // Initialize and start PIO
-    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
-    let installed = pio.install(&program.program).unwrap();
+    let (mut pio, sm0, sm1, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    let short_high_installed = pio.install(&short_high.program).unwrap();
+    let long_high_installed = pio.install(&long_high.program).unwrap();
     let (int, frac) = (0, 0); // as slow as possible (0 is interpreted as 65536)
-    let (mut sm, _, mut tx) = rp2040_hal::pio::PIOBuilder::from_program(installed)
-        .set_pins(led_pin_id, 1)
+    let (mut sm0, _, mut tx1) = rp2040_hal::pio::PIOBuilder::from_program(short_high_installed)
+        .set_pins(other_pin, 1)
         .clock_divisor_fixed_point(int, frac)
         .build(sm0);
 
-    sm.set_pindirs([(led_pin_id, hal::pio::PinDir::Output)]);
-    sm.start();
-    tx.write(100u32);
+    let (mut sm1, _, _) = rp2040_hal::pio::PIOBuilder::from_program(long_high_installed)
+        .set_pins(led_pin_id, 1)
+        .clock_divisor_fixed_point(int, frac)
+        .build(sm1);
+
+    sm0.set_pindirs([(other_pin, hal::pio::PinDir::Output)]);
+    sm0.start();
+
+
+    sm1.set_pindirs([(led_pin_id, hal::pio::PinDir::Output)]);
+    sm1.start();
+    tx1.write(10u32);
     // PIO runs in background, independently from CPU
     loop {
-        defmt::error!("hello");
+        defmt::error!("loop");
     }
 }
